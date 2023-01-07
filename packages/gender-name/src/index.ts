1ed96c89d4e79a699f7999d4ef1e25cf1f852d13
Theryston <fuktodo2@gmail.com>
2023-01-07 13:15:19 -0300

    first commit

---
+++ packages/gender-name/src/index.ts
@@ -0,0 +1,7 @@
+import { Options } from "./types/Options";
+
+async function genderName(name: string, options: Options) {
+  console.log("name", name);
+}
+
+export default genderName;
---
+++ packages/gender-name/src/types/Options.ts
@@ -0,0 +1,3 @@
+export type Options = {
+  model: any;
+};
