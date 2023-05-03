5363be116c4d2e0d89a4de4712f9810b2619a39b
= <=>
2023-05-03 17:45:59 -0300

    Sanitize first name before executing text classification

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,12 +3,20 @@ import { HfInference } from '@huggingface/inference';
 
 @Injectable()
 export class AppService {
+  sanitizeName(name: string): string {
+    const firstName = name.split(/\s+/)[0].replace(/[^a-zA-Z]+/g, '');
+
+    return firstName.toLowerCase();
+  }
+
   async execute(name: string) {
     if (!name) {
       throw new HttpException('Name is required', HttpStatus.BAD_REQUEST);
     }
 
-    if (name.length < 2) {
+    const sanitizedFirstName = this.sanitizeName(name);
+
+    if (sanitizedFirstName.length < 2) {
       throw new HttpException(
         'Name must be at least 2 characters',
         HttpStatus.BAD_REQUEST,
@@ -17,12 +25,12 @@ export class AppService {
 
     const inference = new HfInference(process.env.HF_ACCESS_TOKEN);
     const result = await inference.textClassification({
-      inputs: name,
+      inputs: sanitizedFirstName,
       model: 'theryston/gender-name',
     });
     const mainResult = result.sort((a, b) => b.score - a.score)[0];
     return {
-      name,
+      name: sanitizedFirstName,
       gender: mainResult.label,
       score: mainResult.score,
       full_result: result,
