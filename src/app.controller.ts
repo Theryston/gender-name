d28c947c25afd6ee9f1e39edb7e07bdce11eacb1
= <=>
2023-05-10 10:09:29 -0300

    Add root endpoint to AppController that provides information about the Gender Name API

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,6 +6,26 @@ import { IModel, models } from './models';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Get('/')
+  root() {
+    return {
+      message: 'Welcome to the Gender Name API!',
+      description:
+        "An API that uses artificial intelligence to predict someone's gender based on their name",
+      endpoints: [
+        {
+          path: '/predict?name={name}&model={model}',
+          description:
+            "Predict a person's gender based on their name and selected model. The model is optional",
+        },
+        {
+          path: '/models',
+          description: 'Get a list of all available models',
+        },
+      ],
+    };
+  }
+
   @Get('/predict')
   async predict(@Query('name') name: string, @Query('model') model?: string) {
     return await this.appService.execute(name, model);
