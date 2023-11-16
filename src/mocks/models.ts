0069b44b0dd92fd52eadc7d93d262eaca5df7cf7
= <=>
2023-11-16 20:40:55 -0300

    feat: add model to mock file

--- src/mocks/models.ts
+++ src/mocks/models.ts
@@ -18,7 +18,17 @@ export const models: IModel[] = [
     replicateStableVersion:
       '5fb1931a1bd7f19a2635db60e28499416192cf837e790cf49eb96e121e9d2481',
     githubUrl: 'https://github.com/Theryston/gender-name/tree/main/models/gnbr',
-    country: 'USA',
+    country: 'BR',
     type: 'HUMAN_NAME',
   },
+  {
+    name: 'gnusa',
+    replicateOwnerName: 'theryston',
+    replicateName: 'gnusa',
+    replicatePublicUrl: 'https://replicate.com/theryston/gnusa',
+    replicateStableVersion: '974ded744cc3bc0e1373ac33da558bd7d398f16f6d3623ef2b1889012a0d3d12',
+    githubUrl: 'https://github.com/Theryston/gender-name/tree/main/models/gnusa',
+    country: 'USA',
+    type: 'HUMAN_NAME'
+  }
 ];
